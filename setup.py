c9c8cf4519361c4fa84c6587996c96482f4ee948
Tom English <redsoxfantom@gmail.com>
2019-11-21 04:28:41 -0500

    Added current commit hash to version

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup
+import subprocess
+import os
 
-package_version="0.0.1-TESTING"
+package_version="0.0.1+TESTING"
 with open("VERSION") as f:
     package_version=f.read()
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+try:
+    sha = subprocess.check_output(['git','rev-parse','--short','HEAD'],cwd=cwd).decode('ascii').strip()
+    package_version = package_version + "+" + sha
+except Exception:
+    pass
 
 package_short_description="Sends a wake on lan signal to a computer and waits for it to come online"
 package_long_description=package_short_description
