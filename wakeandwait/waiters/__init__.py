2b02ee80e367fc2699051ba93f3b08e9a1e23cb6
Tom English <redsoxfantom@gmail.com>
2019-11-22 04:32:55 -0500

    Added kwargs to waiter construction

--- wakeandwait/waiters/__init__.py
+++ wakeandwait/waiters/__init__.py
@@ -4,6 +4,6 @@ from wakeandwait.waiters.waiter import WaiterException
 
 def waiter_factory(waiter_type: str, **kwargs) -> Waiter:
     if waiter_type.lower() == 'ping':
-        inst = Ping(kwargs)
+        inst = Ping(**kwargs)
         return inst
     raise WaiterException(F"Waiter Type {waiter_type} not supported")
