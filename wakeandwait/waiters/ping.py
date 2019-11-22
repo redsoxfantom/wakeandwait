23acf3256c175dff564472f196fe80cc59aa9ab8
Tom English <redsoxfantom@gmail.com>
2019-11-22 17:19:05 -0500

    Check for required parameters in Ping

--- wakeandwait/waiters/ping.py
+++ wakeandwait/waiters/ping.py
@@ -1,8 +1,11 @@
 from wakeandwait.waiters.waiter import Waiter
+from wakeandwait.waiters.waiter import WaiterException
 
 class Ping(Waiter):
     def __init__(self,**kwargs):
-        pass
+        if 'ip' not in kwargs:
+            raise WaiterException("'ip' required when using Ping as a waiter")
+        self.target_ip = kwargs['ip']
 
     def wait(self,timeout):
         pass
