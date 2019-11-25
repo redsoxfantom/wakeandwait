f84b49ad1eb4150415e4a221f41b41af2a846a37
Tom English <redsoxfantom@gmail.com>
2019-11-24 20:40:14 -0500

    Load and run waker and waiter

--- wakeandwait/__init__.py
+++ wakeandwait/__init__.py
@@ -1,4 +1,14 @@
 from wakeandwait.waiters import waiter_factory
+from wakeandwait.wakers import waker_factory
 
 def wakeandwait(waker,waiter,**kwargs):
+    waker = waker_factory(waker,**kwargs)
     waiter = waiter_factory(waiter,**kwargs)
+    print(F"Running wakeandwait with waker {waker} and waiter {waiter}")
+    if not waker.wake():
+        print("Waker failed")
+        return False
+    if not waiter.wait():
+        print("Waiter failed")
+        return False
+    return True
